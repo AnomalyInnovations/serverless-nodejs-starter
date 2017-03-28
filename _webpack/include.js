2216c0ad090775a34000b5be3235770ae3690b52
Jay <jvelayud@gmail.com>
2017-03-28 16:21:20 -0400

    Fixing formatting

--- _webpack/include.js
+++ _webpack/include.js
@@ -5,6 +5,6 @@ require('source-map-support').install();
 // Required if the hanlder function is async, as promises
 // can swallow error messages.
 process.on('unhandledRejection', function(e) {
-	console.log(e.stack);
-	process.exit(1);
+  console.log(e.stack);
+  process.exit(1);
 });
