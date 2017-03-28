bcc0764d41d6efbb453c134f712055f7940263e3
Jay <jvelayud@gmail.com>
2017-03-28 16:20:01 -0400

    Adding sourcemaps for proper error messages

---
+++ _webpack/include.js
@@ -0,0 +1,10 @@
+// Add source map support for proper stack traces
+require('source-map-support').install();
+
+// Catch all unhandled exceptions and print their stack trace.
+// Required if the hanlder function is async, as promises
+// can swallow error messages.
+process.on('unhandledRejection', function(e) {
+	console.log(e.stack);
+	process.exit(1);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
   // the webpack config) as an entry
   entry: globEntries('!(webpack.config).js'),
   target: 'node',
+  // Generate sourcemaps for proper error messages
+  devtool: 'source-map',
   // Since 'aws-sdk' is not compatible with webpack,
   // we exclude all node dependencies
   externals: [nodeExternals()],
@@ -29,12 +31,13 @@ module.exports = {
 };
 
 function globEntries(globPath) {
+  var include = './_webpack/include.js';
   var files = glob.sync(globPath);
   var entries = {};
 
   for (var i = 0; i < files.length; i++) {
     var entry = files[i];
-    entries[path.basename(entry, path.extname(entry))] = './' + entry;
+    entries[path.basename(entry, path.extname(entry))] = [include, './' + entry];
   }
 
   return entries;
