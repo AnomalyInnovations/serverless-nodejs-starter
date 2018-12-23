1b6face0785595628ce5d1a8d84126830220a642
Pradip Caulagi <caulagi@gmail.com>
2018-12-23 01:40:30 +0100

    Fix issue with use of callback for async function (#39)

--- handler.js
+++ handler.js
@@ -1,15 +1,13 @@
-export const hello = async (event, context, callback) => {
-  const response = {
+export const hello = async (event, context) => {
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
     }),
   };
-
-  callback(null, response);
 };
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) => 
+const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
   setTimeout(() => {
     resolve(`${rest.copy} (with a delay)`);
   }, time * 1000)
